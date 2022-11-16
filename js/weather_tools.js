0776eb0ac67c091b20799547112c03f067aef42d
easy82 <easy82.contact@gmail.com>
2022-11-16 17:59:37 +0100

    First commit

---
+++ js/weather_tools.js
@@ -0,0 +1,71 @@
+const weatherCodes = {
+	"clear": [ 0 ],
+	"cloudy": [ 1, 2, 3 ],
+	"fog": [ 45, 48 ],
+	"drizzle": [ 51, 53, 55, 56, 57 ],
+	"rain": [ 61, 63, 65, 66, 67 ],
+	"snow": [ 71, 73, 75, 77 ],
+	"shower": [ 80, 81, 82, 85, 86 ],
+	"thunder": [ 95, 96, 99 ],
+};
+
+const weatherIcons = {
+	"clear": { icon: "fa-sun", color: "weather-orange" },
+	"cloudy": { icon: "fa-cloud", color: "weather-grey" },
+	"fog": { icon: "fa-water", color: "weather-grey" },
+	"drizzle": { icon: "fa-cloud-rain", color: "weather-cyan" },
+	"rain": { icon: "fa-cloud-rain", color: "weather-cyan" },
+	"snow": { icon: "fa-snowflake", color: "weather-cyan" },
+	"shower": { icon: "fa-cloud-showers-heavy", color: "weather-grey" },
+	"thunder": { icon: "fa-bolt-lightning", color: "weather-orange" },
+};
+
+fillElement = function( element, data ) {
+	document.getElementById( element ).innerHTML = data.toString();
+}
+
+styleElement = function( element, style ) {
+	document.getElementById( element ).className += " " + style;
+}
+
+rotateElement = function( element, value ) {
+	document.getElementById( element ).style.rotate = value.toString() + "deg";
+}
+
+getFormattedTime = function( datetime ) {
+	return new Date( datetime + "Z" ).toLocaleTimeString( 
+		[], { hour: "2-digit", minute: "2-digit"
+	});
+}
+
+getWeatherType = function( id ) {
+	for ( var name in weatherCodes ) {
+		var codes = weatherCodes[ name ];
+
+		for( var i = 0; i < codes.length; i++ ) {
+			if ( codes[ i ] == id ) {
+				return name;
+			}
+		}
+	}
+	return "";
+}
+
+getNextHourIndexFrom = function( data ) {
+	var dateObj = new Date();
+	dateObj.setTime( dateObj.getTime() + ( 60*60*1000 ));
+
+	var nextISO = dateObj.toISOString();
+	var nextStr = nextISO.slice( 0, nextISO.indexOf( ":" )) + ":00";
+
+	for ( var index = 0; index < data.length; index++ ) {
+		if ( data[ index ] == nextStr ) {
+			return index;
+		}
+	}
+	return -1;
+}
+
+capitalizeString = function( str ) {
+	return str != "" ? str.charAt( 0 ).toUpperCase() + str.slice( 1 ) : "";
+}
---
+++ reports/check_weather.py
@@ -0,0 +1,87 @@
+import requests, json, time
+from os.path import exists, getmtime
+
+
+base_url = "https://api.open-meteo.com/v1/forecast"
+
+geocoord_latitude = 46.908714
+geocoord_longitude = 19.694847
+
+forecast_filename = "weather_forecast.json"
+forecast_expiration = 3600
+
+weather_filename = "current_weather.json"
+weather_expiration = 300
+
+
+def get_weather_report( params ):
+    try:
+        response = requests.get( base_url, params = params )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print( "HTTP error:", e )
+    except requests.exceptions.RequestException as e:
+        print( "Request error:", e )
+    else:
+        return response
+
+
+def save_report_to_file( filename, json_data ):
+    try:
+        json_dump = json.dumps( json_data )
+        json_file = open( filename, "w" )
+    except IOError as e:
+        print( "File IO error:", e )
+    else:
+        json_file.write( json_dump )
+        json_file.close()
+
+
+def is_report_expired( filename, expiration ):
+    if exists( filename ):
+        since_last_update = time.time() - getmtime( filename )
+        if expiration > since_last_update:
+            return False
+    return True
+
+
+def download_forecast():
+    if is_report_expired( forecast_filename, forecast_expiration ):
+        print( "Fetching weather forecast...")
+        response = get_weather_report({ 
+            "latitude": str( geocoord_latitude ), 
+            "longitude": str( geocoord_longitude ),
+            "hourly": [ "temperature_2m", "windspeed_10m", "winddirection_10m", "weathercode" ],
+        })
+        if response:
+            print( "Saving weather forecast...")
+            save_report_to_file( forecast_filename, response.json() )
+
+
+def download_weather():
+    if is_report_expired( weather_filename, weather_expiration ):
+        print( "Fetching current weather...")
+        response = get_weather_report({ 
+            "latitude": str( geocoord_latitude ), 
+            "longitude": str( geocoord_longitude ),
+            "current_weather": True,
+        })
+        if response:
+            print( "Saving current weather...")
+            save_report_to_file( weather_filename, response.json() )
+
+
+download_forecast()
+download_weather()
+
+
+#print( get_forecast_response().json() )
+#print( time.ctime( getmtime( forecast_filename )))
+#print( getmtime( forecast_filename ))
+#print( time.time() )
+#print( time.time() - getmtime( forecast_filename ))
+#print( datetime.datetime.now() + datetime.timedelta( hours=1 ))
+#print( is_forecast_expired() )
+#download_forecast()
+
+
